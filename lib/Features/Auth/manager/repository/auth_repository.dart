0356bff4620e8a67ce0ae7e9eb901c0f93751215
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-01-29 15:13:42 +0200

    register repository

---
+++ lib/Features/Auth/manager/repository/auth_repository.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+import 'package:cloozy/Features/Auth/manager/models/register_model.dart';
+
+class AuthRepository {
+  final String baseUrl = "https://abc0-197-35-86-21B.ngrok-free.app/api";
+
+  Future<RegisterResonse> register(RegisterRequest data) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/users/register'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(data.toJson()),
+    );
+    if (response.statusCode == 200) {
+      return RegisterResonse.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Faild to register: ${response.body}');
+    }
+  }
+}
