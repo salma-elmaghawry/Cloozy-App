99efbe36b24f033edb45541d38b5db7880a468b0
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-02-01 02:33:58 +0200

    ✅Curves

--- lib/Feature/onboarding/custom_clippers.dart
+++ lib/Feature/onboarding/custom_clippers.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 
+//3
 class SymmetricalCurveClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
     path.lineTo(0, size.height * 0.8); // Start at 80% height on the left
 
-    // Symmetrical curve peaking in the center
+    // Adjust the control points for a more balanced curve
     path.quadraticBezierTo(
         size.width / 2,
         size.height * 1.0, // Control point at the center
         size.width,
-        size.height * 0.9); // End at 80% height on the right
+        size.height * 0.8); // End at 80% height on the right
 
     path.lineTo(size.width, 0); // Close the path at the top-right corner
     path.close();
@@ -48,16 +49,19 @@ class WaveCurveClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
-    path.lineTo(0, size.height * 0.7); // Start at 70% height on the left
+    path.lineTo(0, size.height * 0.8); //  80% height on the left
 
-    // Wave-like curve with two control points
-    path.quadraticBezierTo(size.width * 0.25, size.height * 0.9,
-        size.width * 0.5, size.height * 0.7);
-
+    // First control point
     path.quadraticBezierTo(
-        size.width * 0.75, size.height * 0.5, size.width, size.height * 0.7);
+        size.width * 0.25, size.height * 0.6, // Control point
+        size.width * 0.5, size.height * 0.8); // Midpoint
 
-    path.lineTo(size.width, 0);
+    // Second control point: creates a smooth curve downwards
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 1.0, // Control point
+        size.width, size.height * 0.8); // End at 80% height on the right
+
+    path.lineTo(size.width, 0); // Close the path at the top-right corner
     path.close();
     return path;
   }
